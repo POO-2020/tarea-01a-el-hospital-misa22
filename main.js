ed1982dd3af215c4dd6dbb2fa12359ca70fa44c7
Your Name <you@example.com>
2020-02-20 10:44:19 -0600

    class fecha terminada y probada en main

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import Nombre from "./nombre.js"
+import Nombre from "./nombre.js";
+import Fecha from "./fecha.js";
 const nombre1 = new Nombre("Eduardo Misael","Solano","Rolon");
 const probarNombre = _ => console.log(nombre1.getNombreCompleto());
 probarNombre();
@@ -6,3 +7,15 @@ const probarApellido = _ => console.log(nombre1.getApellidoNombre());
 probarApellido ();
 const probarIniciales = _ => console.log(nombre1.getIniciales());
 probarIniciales();
+const fecha1 = new Fecha("25","05","2002");
+const probarFecha = _ =>
+    console.log(
+        fecha1.getAnnios(),
+        fecha1.getDiaFecha(),
+        fecha1.getDias(),
+        fecha1.getFecha(),
+        fecha1.getMeses(),
+        fecha1.getSemanas()
+    );
+    probarFecha();
+    
